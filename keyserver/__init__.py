4264bfed1788676da730f4a5cf03717f253c1b4d
root <root@licface.net>
2015-06-23 21:32:22 +0700

    version: 1.0; test: 0.2

--- keyserver/__init__.py
+++ keyserver/__init__.py
@@ -2,5 +2,7 @@ __VERSION__ 	= "1.0"
 __AUTHOR__  	= "licface"
 __EMAIL__		= "licface@yahoo.com"
 __BUILD__		= "2.7"
-__TEST__		= "0.1"
+__TEST__		= "0.2"
 __PLATFORM__	= "all"
+
+from . import generator
